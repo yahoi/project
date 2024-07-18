ffe7b4861648388a4027f6087cab02e261f89f55
yahoi <prasanth15122001@gmail.com>
2024-07-19 00:01:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# Define your Docker Compose file name
-#COMPOSE_FILE="docker-compose.yml"
-
-# Define the name of the Docker image
 IMAGE_NAME="myreactapp"
 
-# Shut down all containers running using Docker Compose
-#docker-compose down
 docker rm -f `docker ps -aq`
+
 docker rmi -f $(docker images -aq)
 
-
-# Build the Docker image
 docker build -t $IMAGE_NAME .
-
-
-
-
-# Bring up containers using Docker Compose
 
 docker run -itd -p 8000:80 --name prasanth $IMAGE_NAME
 
